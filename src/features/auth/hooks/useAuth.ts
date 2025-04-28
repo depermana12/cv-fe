9af3be78275fc2d65fea31ed5f5ae61ea22439d9
depermana12 <deddiapermana97@gmail.com>
2025-04-28 20:21:52 +0700

    add auth hook

---
+++ src/features/auth/hooks/useAuth.ts
@@ -0,0 +1,11 @@
+import { useAuthStore } from "../store/authStore";
+
+export const useAuth = () => {
+  const user = useAuthStore((state) => state.user);
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  const signIn = useAuthStore((state) => state.signIn);
+  const signUp = useAuthStore((state) => state.signUp);
+  const signOut = useAuthStore((state) => state.signOut);
+
+  return { user, isAuthenticated, signIn, signUp, signOut };
+};
