02502c4b25bbeef9041539cb477d7f589fdf7ce2
depermana12 <deddiapermana97@gmail.com>
2025-05-04 14:52:46 +0700

    add query config for profile

---
+++ src/features/cv/queries/profileQuery.ts
@@ -0,0 +1,12 @@
+import { queryOptions } from "@tanstack/react-query";
+import { profileService } from "../services/personalService";
+
+export const profileQuery = (userId: number, service = profileService) =>
+  queryOptions({
+    queryKey: ["profile", userId],
+    queryFn: async () => {
+      const res = await service.get(userId);
+      return res.data.data;
+    },
+    staleTime: 1000 * 60 * 5, // 5 minutes
+  });
