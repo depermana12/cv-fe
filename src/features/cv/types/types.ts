08f927ed04a6d76a96a7e6702a07ae654472f832
depermana12 <deddiapermana97@gmail.com>
2025-05-11 15:21:18 +0700

    feat: add types form dto of work

--- src/features/cv/types/types.ts
+++ src/features/cv/types/types.ts
@@ -7,12 +7,16 @@ import {
   educationCreateSchema,
   educationSchema,
 } from "../schema/educationSchema";
+import { workCreateSchema, workSchema } from "../schema/workSchema";
 
 export type PersonalForm = z.infer<typeof personalCreateSchema>;
 export type PersonalDTO = z.infer<typeof personalSelectSchema>;
 
 export type EducationForm = z.infer<typeof educationCreateSchema>;
 export type EducationDTO = z.infer<typeof educationSchema>;
+
+export type WorkForm = z.infer<typeof workCreateSchema>;
+export type WorkDTO = z.infer<typeof workSchema>;
 
 export type ApiResponse<T> = {
   success: boolean;
