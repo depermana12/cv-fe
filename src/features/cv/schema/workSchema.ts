16dc13bc2f97b8255145fbd49f1aa578f0dda1e2
depermana12 <deddiapermana97@gmail.com>
2025-05-11 15:19:06 +0700

    feat: add work schema

---
+++ src/features/cv/schema/workSchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const workSelectSchema = z.object({
+  id: z.number().int().positive(),
+  personalId: z.number().int().positive(),
+  company: z.string().max(100, { message: "Must be 100 characters or fewer" }),
+  position: z.string().max(100, { message: "Must be 100 characters or fewer" }),
+  startDate: z.coerce.date({ invalid_type_error: "Invalid start date" }),
+  endDate: z.coerce.date({ invalid_type_error: "Invalid end date" }),
+  url: z.string().url({ message: "Invalid URL format" }),
+  isCurrent: z.boolean(),
+});
+
+export const workCreateSchema = workSelectSchema.omit({
+  id: true,
+});
