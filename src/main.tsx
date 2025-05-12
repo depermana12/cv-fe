0af72803ac5881ff6e36c5a64580358eb515b5e9
depermana12 <deddiapermana97@gmail.com>
2025-05-12 23:05:14 +0700

    add mantine notification

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,9 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { routeTree } from "./routeTree.gen";
 import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 import "./index.css";
 import { useAuthStore } from "./features/auth/store/authStore";
 
@@ -40,6 +42,7 @@ const InnerApp = () => {
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider>
+      <Notifications />
       <QueryClientProvider client={queryClient}>
         <InnerApp />
       </QueryClientProvider>
