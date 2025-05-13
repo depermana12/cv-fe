38bcdb023991cd362f98e722899aafd789234665
depermana12 <deddiapermana97@gmail.com>
2025-05-13 11:33:43 +0700

    refactor: add mantine notification

--- src/features/cv/hooks/useUpdatePersonal.ts
+++ src/features/cv/hooks/useUpdatePersonal.ts
@@ -1,19 +1,34 @@
 import { useMutation } from "@tanstack/react-query";
 import { profileService } from "../services/personalService";
 import { PersonalForm } from "../types/types";
+import { notifications } from "@mantine/notifications";
 
-// TODO: add mantine notification
 export const useUpdatePersonal = () => {
   return useMutation({
     mutationFn: async ({ id, ...data }: { id: number } & PersonalForm) => {
       const res = await profileService.patch(id, data);
       return res.data.data;
     },
-    onSuccess: () => {
-      console.log("Personal info upadated");
+    onSuccess: (data) => {
+      notifications.show({
+        position: "top-center",
+        withCloseButton: true,
+        autoClose: 3000,
+        title: `Personal info for ${data.fullName} saved`,
+        message: "Your personal information has been saved successfully.",
+        color: "green",
+      });
     },
     onError: (err) => {
-      console.error("Failed to update personal info", err);
+      notifications.show({
+        position: "top-center",
+        withCloseButton: true,
+        autoClose: 3000,
+        title: "Failed to save personal info",
+        message: "There was an error saving your personal information.",
+        color: "red",
+      });
+      console.error("Failed to save personal info", err);
     },
   });
 };
