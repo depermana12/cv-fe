c69e158a6174cca84ddae2204ced54b53918eae4
depermana12 <deddiapermana97@gmail.com>
2025-05-17 16:18:47 +0700

    setup mantine theme obj

---
+++ src/themes/blao.ts
@@ -0,0 +1,25 @@
+import { createTheme } from "@mantine/core";
+
+export const myTheme = createTheme({
+  colors: {
+    blue: [
+      "#e6f4ff",
+      "#d0e2ff",
+      "#a6c8ff",
+      "#78a9ff",
+      "#4589ff",
+      "#0f62fe",
+      "#0043ce",
+      "#002d9c",
+      "#001d6c",
+      "#001141",
+    ],
+  },
+  primaryColor: "blue",
+  primaryShade: 5,
+  defaultRadius: "md",
+  focusRing: "auto",
+  fontFamily: "Open Sans, sans-serif",
+  fontFamilyMonospace: "Monaco, Courier, monospace",
+  headings: { fontFamily: "Open Sans, sans-serif" },
+});
