1a3bf21c2db53f9b60df86a60161a99b535b0c89
depermana12 <deddiapermana97@gmail.com>
2025-05-17 16:22:29 +0700

    refactor to use Box mantine instead of div

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
-import { Link, Outlet } from "@tanstack/react-router";
+import { Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Header } from "./components/ui/Header";
+import { Box } from "@mantine/core";
 
 function App() {
   return (
-    <div className="h-dvh w-dvw flex flex-col bg-gray-50">
-      <nav className="p-4 shadow bg-white flex justify-between items-center">
-        <div className="text-lg font-bold">
-          <Link to="/">Kodedroid CV</Link>
-        </div>
-        <div className="space-x-4">
-          <Link to="/auth/signin" className="text-blue-600 hover:underline">
-            Sign In
-          </Link>
-          <Link to="/auth/signup" className="text-blue-600 hover:underline">
-            Sign Up
-          </Link>
-        </div>
-      </nav>
-
-      <div className="flex-1 flex items-center justify-center p-4">
+    <Box bg="gray.0" className="min-h-dvh min-w-dvw flex flex-col">
+      <Header />
+      <Box
+        component="main"
+        className="flex-1 flex items-center justify-center p-4"
+      >
         <Outlet />
-      </div>
+      </Box>
 
       <TanStackRouterDevtools />
       <ReactQueryDevtools />
-    </div>
+    </Box>
   );
 }
 
