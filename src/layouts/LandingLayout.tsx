0549afe2206fff9211b5873c0275cba655a4e356
depermana12 <deddiapermana97@gmail.com>
2025-05-18 00:41:57 +0700

    feat: add LandingLayout for homepage

---
+++ src/layouts/LandingLayout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "@tanstack/react-router";
+import { Box, useMantineColorScheme, useMantineTheme } from "@mantine/core";
+
+import { Header } from "../components/ui/Header";
+
+const LandingLayout = ({ children }: { children: ReactNode }) => {
+  const theme = useMantineTheme();
+  const { colorScheme } = useMantineColorScheme();
+
+  return (
+    <Box
+      bg={colorScheme === "dark" ? theme.colors.dark[7] : theme.colors.gray[0]}
+      className="min-h-screen min-w-dvw flex flex-col"
+    >
+      <Header />
+      <Box component="main">{children}</Box>
+    </Box>
+  );
+};
+export default LandingLayout;
