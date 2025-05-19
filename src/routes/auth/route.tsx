26f9be4f8f9a4e3751bb342a859ef68a4721026d
depermana12 <deddiapermana97@gmail.com>
2025-05-19 08:16:07 +0700

    add auth layout wrapper for auth route

--- src/routes/auth/route.tsx
+++ src/routes/auth/route.tsx
@@ -1,18 +1,6 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
+import { AuthLayout } from "../../layouts/AuthLayout";
 
 export const Route = createFileRoute("/auth")({
   component: AuthLayout,
 });
-
-function AuthLayout() {
-  return (
-    <div className="flex items-center justify-center">
-      <div className="w-full max-w-md p-8 space-y-4 bg-white shadow rounded">
-        <h1 className="text-2xl font-bold text-center">
-          Welcome to Kodedroid CV
-        </h1>
-        <Outlet />
-      </div>
-    </div>
-  );
-}
