48ee5cef343455e26ff4a778ca649934017a15d5
depermana12 <deddiapermana97@gmail.com>
2025-05-19 08:21:26 +0700

    refactor: add wrapper for sign up page

--- src/features/auth/pages/SignUpPage.tsx
+++ src/features/auth/pages/SignUpPage.tsx
@@ -1,9 +1,18 @@
+import { Container, Paper, Title, Text } from "@mantine/core";
 import SignUpForm from "../components/SignUpForm";
 
 export const SignUpPage = () => {
   return (
-    <div className="mt-20">
-      <SignUpForm />
-    </div>
+    <Container size={420} my={40}>
+      <Title ta="center" fw="bold">
+        Sign up for myResume!
+      </Title>
+      <Text c="dimmed" size="sm" ta="center" mt="md">
+        Fill out the form and let's get started
+      </Text>
+      <Paper withBorder shadow="sm" p="xl" mt={30} radius="md">
+        <SignUpForm />
+      </Paper>
+    </Container>
   );
 };
