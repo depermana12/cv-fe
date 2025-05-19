0e94668fa8a42b2e9763418ec9ad7d2708a88470
depermana12 <deddiapermana97@gmail.com>
2025-05-19 08:13:29 +0700

    refactor: add wrapper for sign in page ui

--- src/features/auth/pages/SignInPage.tsx
+++ src/features/auth/pages/SignInPage.tsx
@@ -1,9 +1,22 @@
+import { Container, Title, Text, Anchor, Paper } from "@mantine/core";
 import SignInForm from "../components/SignInForm";
+import { Link } from "@tanstack/react-router";
 
 export const SignInPage = () => {
   return (
-    <div className="mt-20">
-      <SignInForm />
-    </div>
+    <Container size={420} my={40}>
+      <Title ta="center" fw="bold">
+        Welcome back!
+      </Title>
+      <Text c="dimmed" size="sm" ta="center" mt="md">
+        Do not have an account yet?{" "}
+        <Anchor variant="gradient" component={Link} to="/auth/signup">
+          Create account
+        </Anchor>
+      </Text>
+      <Paper withBorder shadow="sm" p="xl" mt={30} radius="md">
+        <SignInForm />
+      </Paper>
+    </Container>
   );
 };
