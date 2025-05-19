dd639308f33ce90adcc1637cb706bd87a398f8da
depermana12 <deddiapermana97@gmail.com>
2025-05-19 08:08:40 +0700

    refactor: signin hook add notification and callback onsuccess onerror

--- src/features/auth/hooks/useSignIn.ts
+++ src/features/auth/hooks/useSignIn.ts
@@ -1,13 +1,38 @@
 import { useMutation } from "@tanstack/react-query";
-import { SignIn } from "../types/auth.schema";
-import { useAuthStore } from "../store/authStore";
 import { useNavigate } from "@tanstack/react-router";
+import { notifications } from "@mantine/notifications";
+
+import { useAuthStore } from "../store/authStore";
+import { SignIn } from "../types/auth.schema";
 
 export const useSignIn = () => {
   const { signIn } = useAuthStore();
   const navigate = useNavigate();
+
   return useMutation({
     mutationFn: async (inputSignIn: SignIn) => signIn(inputSignIn),
-    onSuccess: () => navigate({ to: "/dashboard" }),
+    onSuccess: (user) => {
+      notifications.show({
+        position: "top-right",
+        withCloseButton: true,
+        autoClose: 3000,
+        title: "Login successful",
+        message: `Welcome back, ${user.username}!`,
+        color: "green",
+      });
+      navigate({ to: "/dashboard" });
+    },
+    onError: (error) => {
+      notifications.show({
+        position: "top-center",
+        withCloseButton: true,
+        autoClose: 3000,
+        title: "Login failed",
+        message:
+          error instanceof Error ? error.message : "Invalid email or password",
+        color: "red",
+      });
+      console.error("Login error:", error);
+    },
   });
 };
