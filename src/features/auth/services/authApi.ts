3ea94783992271eb20a46169478d87f78568443e
depermana12 <deddiapermana97@gmail.com>
2025-06-15 17:21:19 +0700

    refactor(auth): wrap to class and add other endpoint reflecting the the backend

--- src/features/auth/services/authApi.ts
+++ src/features/auth/services/authApi.ts
@@ -1,16 +1,65 @@
 import { axiosClient } from "@lib/axiosClient";
-import { SignIn, SignUp } from "../types/auth.schema";
+import {
+  ResetPassword,
+  ForgetPassword,
+  SignIn,
+  SignUp,
+} from "../types/auth.schema";
 import { AuthApiResponse } from "../types/types";
 import { AxiosResponse } from "axios";
 
-export const signUpService = <T>(
-  inputSignUp: SignUp,
-): Promise<AxiosResponse<AuthApiResponse<T>>> => {
-  return axiosClient.post<AuthApiResponse<T>>("/auth/signup", inputSignUp);
-};
+export class AuthApi {
+  private base = "/auth";
 
-export const signInService = <T>(
-  inputSignIn: SignIn,
-): Promise<AxiosResponse<AuthApiResponse<T>>> => {
-  return axiosClient.post<AuthApiResponse<T>>("/auth/signin", inputSignIn);
-};
+  signUp<T>(inputData: SignUp): Promise<AxiosResponse<AuthApiResponse<T>>> {
+    return axiosClient.post<AuthApiResponse<T>>(
+      `${this.base}/signup`,
+      inputData,
+    );
+  }
+
+  signIn<T>(inputData: SignIn): Promise<AxiosResponse<AuthApiResponse<T>>> {
+    return axiosClient.post<AuthApiResponse<T>>(
+      `${this.base}/signin`,
+      inputData,
+    );
+  }
+
+  forgetPassword<T>(
+    inputData: ForgetPassword,
+  ): Promise<AxiosResponse<AuthApiResponse<T>>> {
+    return axiosClient.post<AuthApiResponse<T>>(
+      `${this.base}/forget-password`,
+      inputData,
+    );
+  }
+  resetPassword<T>(
+    token: string,
+    newPassword: ResetPassword,
+  ): Promise<AxiosResponse<AuthApiResponse<T>>> {
+    return axiosClient.post<AuthApiResponse<T>>(
+      `${this.base}/reset-password/${token}`,
+      newPassword,
+    );
+  }
+  verifyEmail<T>(token: string): Promise<AxiosResponse<AuthApiResponse<T>>> {
+    return axiosClient.post<AuthApiResponse<T>>(
+      `${this.base}/verify-email/${token}`,
+    );
+  }
+  emailVerificationStatus<T>(
+    userId: number,
+  ): Promise<AxiosResponse<AuthApiResponse<T>>> {
+    return axiosClient.get<AuthApiResponse<T>>(
+      `${this.base}/email-verification-status/${userId}`,
+    );
+  }
+  sendEmailVerification<T>(): Promise<AxiosResponse<AuthApiResponse<T>>> {
+    return axiosClient.post<AuthApiResponse<T>>(
+      `${this.base}/send-email-verification`,
+    );
+  }
+  refreshToken<T>(): Promise<AxiosResponse<AuthApiResponse<T>>> {
+    return axiosClient.post<AuthApiResponse<T>>(`${this.base}/refresh-token`);
+  }
+}
