c62c000cf70a871ef72b1084ab8ba4e8518dcaf3
depermana12 <deddiapermana97@gmail.com>
2025-06-16 07:51:52 +0700

    add generic resource api class

---
+++ src/features/cv/services/ResourceApi.ts
@@ -0,0 +1,34 @@
+import { axiosClient } from "../../../lib/axiosClient";
+import type { ApiResponse } from "../types/types";
+import { AxiosResponse } from "axios";
+
+export class ResourceApi<T, I> {
+  protected resource: string;
+
+  constructor(resource: string) {
+    this.resource = resource;
+  }
+
+  async post(data: I): Promise<AxiosResponse<ApiResponse<T>>> {
+    return axiosClient.post<ApiResponse<T>>(this.resource, data);
+  }
+
+  async get(id: number): Promise<AxiosResponse<ApiResponse<T>>> {
+    return axiosClient.get<ApiResponse<T>>(`${this.resource}/${id}`);
+  }
+
+  async getAll(): Promise<AxiosResponse<ApiResponse<T[]>>> {
+    return axiosClient.get<ApiResponse<T[]>>(this.resource);
+  }
+
+  async patch(
+    id: number,
+    data: Partial<I>,
+  ): Promise<AxiosResponse<ApiResponse<T>>> {
+    return axiosClient.patch<ApiResponse<T>>(`${this.resource}/${id}`, data);
+  }
+
+  async delete(id: number): Promise<AxiosResponse<ApiResponse<void>>> {
+    return axiosClient.delete<ApiResponse<void>>(`${this.resource}/${id}`);
+  }
+}
