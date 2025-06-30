89eee41d8b20fb3c39deb3273fe94cfd6bdffdf5
depermana12 <deddiapermana97@gmail.com>
2025-06-30 14:57:59 +0700

    feat: compose homepage section

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,16 +1,25 @@
-import { Box } from "@mantine/core";
+import { Box, Divider } from "@mantine/core";
 import { FeaturesCards } from "./components/FeatureCards";
 import { HeroBullets } from "./components/HeroBullets";
+import { FAQ } from "./components/FAQ";
 import { Footer } from "../../components/layout/Footer";
-// import HomeBanner from "../components/ui/HomeBanner";
+import { CTA } from "./components/CTA";
 
 const HomePage = () => {
+  const deviderProps = {
+    variant: "dashed",
+    color: "gray.8",
+    my: "xl",
+    opacity: 0.3,
+  };
   return (
     <Box>
       <HeroBullets />
-      <Box py="xl">
-        <FeaturesCards />
-      </Box>
+      <Divider {...deviderProps} style={{ maxWidth: "70%", margin: "auto" }} />
+      <FeaturesCards />
+      <Divider {...deviderProps} style={{ maxWidth: "70%", margin: "auto" }} />
+      <FAQ />
+      <CTA />
       <Footer />
     </Box>
   );
