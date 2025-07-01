725722bbdac032ee5d4d7b1f75d403c308d95914
depermana12 <deddiapermana97@gmail.com>
2025-07-01 10:35:26 +0700

    refactor: move guard dashboard into seperated function

--- src/routes/dashboard/route.tsx
+++ src/routes/dashboard/route.tsx
@@ -1,17 +1,8 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { DashboardLayout } from "../../layouts/DashboardLayout";
+import { authProtectedGuard } from "../../features/auth/guards/authProtectedGuard";
 
 export const Route = createFileRoute("/dashboard")({
-  beforeLoad: async ({ context, location }) => {
-    if (!context.auth.isAuthenticated) {
-      throw redirect({
-        to: "/auth/signin",
-        search: {
-          redirect: location.href,
-        },
-        replace: true,
-      });
-    }
-  },
+  beforeLoad: () => authProtectedGuard(),
   component: DashboardLayout,
 });
