741bcef83d49d35591a9eceb992e5e17e55f4447
depermana12 <deddiapermana97@gmail.com>
2025-07-04 21:03:31 +0700

    feat(cv): add query for cv paginated query

--- src/features/cv/hooks/useCvs.ts
+++ src/features/cv/hooks/useCvs.ts
@@ -1,4 +1,8 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { cvsQuery } from "./query";
+import { cvsQuery, cvsPaginatedQuery } from "./query";
+import type { CvQueryOptions } from "../types/types";
 
 export const useCvs = () => useSuspenseQuery(cvsQuery());
+
+export const useCvsPaginated = (options?: CvQueryOptions) =>
+  useSuspenseQuery(cvsPaginatedQuery(options));
