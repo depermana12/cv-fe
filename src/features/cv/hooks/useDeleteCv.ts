5d7ef2b6df0127c40542467e4922dfb648dedba1
depermana12 <deddiapermana97@gmail.com>
2025-07-04 21:04:07 +0700

    feat(cv): add mutation hook for deleting cv

---
+++ src/features/cv/hooks/useDeleteCv.ts
@@ -0,0 +1,36 @@
+import { useMutation } from "@tanstack/react-query";
+import { queryClient } from "../../../lib/queryClient";
+import { cvApi } from "../services/CvApi";
+import { notifications } from "@mantine/notifications";
+
+export const useDeleteCv = () => {
+  return useMutation({
+    mutationFn: async (cvId: number) => {
+      const res = await cvApi.delete(cvId);
+      return res.data;
+    },
+    onSuccess: (_data, cvId) => {
+      queryClient.invalidateQueries({ queryKey: ["cvs"] });
+      queryClient.removeQueries({ queryKey: ["cvs", cvId] });
+      notifications.show({
+        position: "top-center",
+        withCloseButton: true,
+        autoClose: 3000,
+        title: "CV deleted",
+        message: "Your CV has been deleted successfully.",
+        color: "green",
+      });
+    },
+    onError: (err) => {
+      notifications.show({
+        position: "top-center",
+        withCloseButton: true,
+        autoClose: 3000,
+        title: "Failed to delete CV",
+        message: "There was an error deleting your CV.",
+        color: "red",
+      });
+      console.error("Failed to delete CV", err);
+    },
+  });
+};
