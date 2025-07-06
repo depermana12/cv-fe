0aa46bf0ec7a2acdb36182e61cce689fea86d4bc
depermana12 <deddiapermana97@gmail.com>
2025-07-06 15:31:07 +0700

    feat(dashboard): add cv dir route inside dashboard

--- src/routes/dashboard/cv/route.tsx
+++ src/routes/dashboard/cv/route.tsx
@@ -3,6 +3,6 @@ import { createFileRoute, Outlet } from "@tanstack/react-router";
 export const Route = createFileRoute("/dashboard/cv")({
   component: () => <Outlet />,
   staticData: {
-    breadcrumb: " CV Builder",
+    breadcrumb: "cv",
   },
 });
