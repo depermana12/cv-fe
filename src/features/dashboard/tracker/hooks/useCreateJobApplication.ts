618b64ba1b4d3afef5d0fcd0bf5bf04877942d78
depermana12 <deddiapermana97@gmail.com>
2025-07-10 10:58:18 +0700

    feat(job tracker): add mutation to create entry

---
+++ src/features/dashboard/tracker/hooks/useCreateJobApplication.ts
@@ -0,0 +1,28 @@
+import { useMutation } from "@tanstack/react-query";
+import { queryClient } from "../../../../lib/queryClient";
+import { JobTrackerCreate } from "../types/jobTracker.type";
+import { notifications } from "@mantine/notifications";
+import { jobTrackerService } from "../services/jobTrackerService";
+
+export const useCreateJobApplication = () => {
+  return useMutation({
+    mutationFn: async (data: JobTrackerCreate) => {
+      const res = await jobTrackerService.post(data);
+      return res.data.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["job-applications"] });
+    },
+    onError: (err) => {
+      notifications.show({
+        position: "top-center",
+        withCloseButton: true,
+        autoClose: 3000,
+        title: "Failed to add job application",
+        message: err?.message || "There was an error adding job application.",
+        color: "red",
+      });
+      console.error("Failed to add", err);
+    },
+  });
+};
