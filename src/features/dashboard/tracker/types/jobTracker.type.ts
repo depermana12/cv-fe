92e06e4d3ff2313450fc0bd363431191672283ac
depermana12 <deddiapermana97@gmail.com>
2025-07-12 15:00:06 +0700

    feat(job tracker): add types props modal

--- src/features/dashboard/tracker/types/jobTracker.type.ts
+++ src/features/dashboard/tracker/types/jobTracker.type.ts
@@ -1,12 +1,14 @@
 import { z } from "zod";
 import {
   jobTrackerCreateSchema,
+  jobTrackerImportSchema,
   jobTrackerQueryOptionsSchema,
   jobTrackerSchema,
   jobTrackerUpdateSchema,
 } from "../schema/jobTracker";
 
 export type JobTracker = z.infer<typeof jobTrackerSchema>;
+export type JobTrackerImport = z.infer<typeof jobTrackerImportSchema>;
 export type JobTrackerCreate = z.infer<typeof jobTrackerCreateSchema>;
 export type JobTrackerUpdate = Partial<z.infer<typeof jobTrackerUpdateSchema>>;
 export type JobTrackerQueryOptions = z.infer<
@@ -30,9 +32,23 @@ export type JobApplicationsResponse = {
   };
 };
 
+export type JobApplicationsTableProps = {
+  applications: JobTracker[];
+  loading?: boolean;
+  onEdit: (application: JobTracker) => void;
+  onDelete: (application: JobTracker) => void;
+  onCreateNew: () => void;
+};
+
 export type JobApplicationFormProps = {
   opened: boolean;
   onClose: () => void;
   initialData?: Partial<JobTrackerCreate>;
   mode?: "create" | "edit";
+};
+
+export type DeleteJobApplicationProps = {
+  opened: boolean;
+  onClose: () => void;
+  application: JobTracker | null;
 };
