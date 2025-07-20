859bef8591f67c9c2a64d8211c2e43859ee64ffe
depermana12 <deddiapermana97@gmail.com>
2025-07-20 22:35:56 +0700

    add types for paginate response

--- src/features/types/types.ts
+++ src/features/types/types.ts
@@ -3,3 +3,14 @@ export type ApiResponse<T> = {
   message: string;
   data: T;
 };
+
+export type PaginatedApiResponse<T> = {
+  success: boolean;
+  message: string;
+  data: T;
+  pagination: {
+    total: number;
+    limit: number;
+    offset: number;
+  };
+};
