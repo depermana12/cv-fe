6a16c5e56d39e6183e5e3a22d8a6d96d5773e8eb
depermana12 <deddiapermana97@gmail.com>
2025-07-29 00:10:30 +0700

    refactor: remove unused module declarations

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />
-declare module "@lib/*";
-declare module "@components/*";
-declare module "@features/*";
-declare module "@routes/*";
-declare module "@assets/*";
-declare module "@lib/axiosClient" {
-  import { AxiosClient } from "axios";
-  export const axiosClient: AxiosInstance;
-}
